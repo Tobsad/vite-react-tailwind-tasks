542cdc8a7505c583f4d586bd62cd433f1bfb8300
Said Eslander <saideslander@gmail.com>
2023-03-12 12:55:35 -0500

    first commit

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -11,7 +11,7 @@ function TaskList() {
   return (
     <div  className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} />
+        <TaskCard key={task.id} task={task}  />
       ))}
     </div>
   );
