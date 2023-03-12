56a943106fe6edc67d51d0c3d38f31aff109ccad
Said Eslander <saideslander@gmail.com>
2023-03-12 01:52:14 -0500

    agregando vite-react-gh pages en master osea el deploy correcto 2.1

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -6,7 +6,7 @@ function TaskList() {
   const { tasks } = useContext(TaskContext);
 
   if (tasks.length === 0) {
-    return <h1 className="text-white text-3xl font-bold text-center pt-16 pb-72 sm:py-12 md:py-12 lg:py-12 ">Sin tareas.</h1>;
+    return <h1 className="text-white text-3xl font-bold text-center pt-16 pb-72 sm:py-12 md:py-12 lg:py-12 ">Sin tareas. </h1>;
   }
   return (
     <div  className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
